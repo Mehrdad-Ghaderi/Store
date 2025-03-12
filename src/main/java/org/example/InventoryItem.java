9085bc2f09a2d7388fa4ad05c8905cc3cc8cf6e2
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2025-03-12 13:52:05 -0400

    InventoryItem initial fields added

--- src/main/java/org/example/InventoryItem.java
+++ src/main/java/org/example/InventoryItem.java
@@ -6,4 +6,13 @@ package org.example;
  * Time: 12:54 PM
  */
 public class InventoryItem {
+    private Product product;
+    private int qytTotal; //currently in stock
+    private int qtyReserved; //products that's in the carts, but not yet sold
+    private int qtyReorder; //the number to order a product when low
+    private int qtyLow; //the trigger or threshold to order more product
+    private double salesPrice;
+
+    /*
+    * reserveItem, releaseItem, sellItem, placeInventoryOrder */
 }
