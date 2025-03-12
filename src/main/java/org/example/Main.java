e23a83b9f16ce553d82cd14551d88b751e8828b1
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2025-03-12 12:51:48 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * Created by Mehrdad Ghaderi, S&M
+ * Date: 3/12/2025
+ * Time: 12:26 PM
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
