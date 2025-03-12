7e4a4c6c3d4f0f86549c43a036d011ecf5ad9b61
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2025-03-12 13:45:20 -0400

    product initial fields added

---
+++ src/main/java/org/example/Category.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * Created by Mehrdad Ghaderi, S&M
+ * Date: 3/12/2025
+ * Time: 1:43 PM
+ */
+public enum Category {
+    PRODUCE, DAIRY, GROCERY
+}
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import java.util.Set;
+
 /**
  * Created by Mehrdad Ghaderi, S&M
  * Date: 3/12/2025
  * Time: 12:54 PM
  */
 public class Product {
+    private String stockKeepingUnit;
+    private String name;
+    private String manufacturer;
+    private Set<Category> categories;
 }
